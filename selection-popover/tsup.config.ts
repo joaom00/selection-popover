57a95660941d363d0ca17741b996e6167e0eeb28
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2023-04-13 12:40:29 -0300

    build: add sourcemap

--- selection-popover/tsup.config.ts
+++ selection-popover/tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   minify: true,
   target: 'es2018',
   external: ['react', 'react-dom'],
-  sourcemap: false,
+  sourcemap: true,
   dts: true,
   format: ['esm', 'cjs'],
   loader: {
